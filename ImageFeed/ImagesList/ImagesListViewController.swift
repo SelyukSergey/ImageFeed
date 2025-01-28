c3e0af711e8d3550741d4e382d794c2949f31c4e
SelyukSergey <selyuk_s@icloud.com>
2025-01-28 18:48:58 +0300

    Свёрстан Launch Screen приложения Свёрстан главный экран с лентой изображений

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+}
+
